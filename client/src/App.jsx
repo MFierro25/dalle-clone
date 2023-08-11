42022c4d52831b51b9af710cf61108529e585657
Miguel Fierro <emfierro2@gmail.com>
2023-08-10 17:25:48 -0700

    nav and route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { logo } from './assets'
+
+import { Home, CreatePost } from './pages'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello!
-    </h1>
+    <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b[#e6ebf4]" >
+      <Link to='/'>
+        <img src={logo} alt="logo" className='w-32 object-contain'/>
+      </Link>
+      <Link to="/create-post" className='font-inter font-medium bg-[#1366c5] text-white hover:bg-sky-500 px-6 py-2 rounded-xl' >
+        Create
+      </Link>
+    </header>
+    <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+    </BrowserRouter>
   )
 }
 
---
+++ notes.md
@@ -0,0 +1,1 @@
+- set hover button state and save 
