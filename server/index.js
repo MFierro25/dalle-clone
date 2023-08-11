8933d8b7b1f0413e434c47930384cc65297893a6
Miguel Fierro <emfierro2@gmail.com>
2023-08-11 14:51:50 -0700

    routes set up

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config();
 
@@ -10,6 +12,9 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+// routes
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
   res.status(200).json({
