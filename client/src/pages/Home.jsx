7b8f6a696d3ff19f130b7022dee5b0a768f18f48
Miguel Fierro <emfierro2@gmail.com>
2023-08-15 18:33:28 -0700

    server setup

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         setLoading(true);
     
         try {
-          const response = await fetch('http://localhost:8080/api/v1/post', {
+          const response = await fetch('https://dall-e-cr0h.onrender.com', {
             method: 'GET',
             headers: {
               'Content-Type': 'application/json',
